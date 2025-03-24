8876da043a8caf31c8109bc3b68992e802a8d7dc
Ansari Mohd Saad <ansarisaad777666@gmail.com>
2025-03-25 02:30:06 +0530

    Inheritance Assignment

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -3,7 +3,7 @@ package com.driver;
 public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
-        super(name,4,4,6,isManual,"F1",1);
+        super(name,4,4,6,isManual,"racing",1);
         //Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats)
     }
 
